9adbf2a339d9c060a4464e4e0dda190b3bb3e293
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-06-30 20:16:28 +0300

    Player findAll filtering through JpaSpecificationExecutor

--- src/main/java/com/game/repository/PlayerRepository.java
+++ src/main/java/com/game/repository/PlayerRepository.java
@@ -2,7 +2,8 @@ package com.game.repository;
 
 import com.game.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PlayerRepository extends JpaRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {
 
 }
